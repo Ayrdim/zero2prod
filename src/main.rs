90c9fa37e58175ad8472fd48f49b40b51be1fa6a
Ryan Dimsey <ryandimsey@gmail.com>
2023-05-11 20:38:33 +1000

    Added initial github actions, lld linker config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// cargo watch -x check -x test -x run
+// cargo tarpaulin --ignore-tests
+// cargo clippy -- -D warnings
+// #[allow(clippy::lint_name)] above code sections to prevent clippy from causing errors on warnings we dont care about/ cant avoid
+// cargo fmt
+// cargo fmt -- --check <-- checks if the format is good, will cause the CI to fail if it isn't good
+// cargo audit <-- will check if any of our dependencies have any known vulnerabilities
+
 fn main() {
     println!("Hello, world!");
 }
