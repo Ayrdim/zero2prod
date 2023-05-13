102cd4587650539d058d6f79c944177861d5f22c
Ryan Dimsey <ryandimsey@gmail.com>
2023-05-13 20:19:39 +1000

    working hello world endpoint example

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,21 @@
 // cargo fmt -- --check <-- checks if the format is good, will cause the CI to fail if it isn't good
 // cargo audit <-- will check if any of our dependencies have any known vulnerabilities
 
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
