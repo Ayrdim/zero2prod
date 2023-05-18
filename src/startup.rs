7811388b3e57d5cd6760e09c354784160b54c113
Ryan Dimsey <ryandimsey@gmail.com>
2023-05-18 21:01:24 +1000

    Tests pass and re ran cargo fmt

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use crate::routes::{health_check, subscribe};
 
-use actix_web::{App, HttpServer, web, dev::Server};
+use actix_web::{dev::Server, web, App, HttpServer};
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
